456ce6bc40e01f98caade74426e4899f2236cf38
Sawadogo Marcus <marcusawen@gmail.com>
2023-03-25 20:15:39 +0100

    all

---
+++ scoreboard.py
@@ -0,0 +1,35 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 20, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.highest_score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} __ Highest Score_ {self.highest_score}", align=ALIGNMENT, font=FONT)
+        
+    def reset(self):
+        if self.score > self.highest_score:
+            self.highest_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+            
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
